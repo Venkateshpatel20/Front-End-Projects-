3065bf776ac01f3c4805f9850c74f57673a20eb5
Venkateshpatel20 <venkateshpatel18@gmail.com>
2026-06-13 11:57:29 +0530

    chore(script.js):handle multiple button states

--- Tic-Tac-Toe-Game/script.js
+++ Tic-Tac-Toe-Game/script.js
@@ -4,12 +4,11 @@ let new_game = document.querySelector("#new-btn");
 let msg = document.querySelector("#msg");
 let msg_container = document.querySelector(".msg-container");
 
-
 // Accessing player's turn
 let turn1 = true;
-let count = 0 //to track draw
+let count = 0; //to track draw
 
-// the positions in which a player wins 
+// the positions in which a player wins
 const winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
@@ -18,8 +17,8 @@ const winPatterns = [
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
-  [6, 7, 8]
-]
+  [6, 7, 8],
+];
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
@@ -27,8 +26,7 @@ boxes.forEach((box) => {
     if (turn1) {
       box.innerHTML = "X";
       turn1 = false;
-    }
-    else {
+    } else {
       box.innerHTML = "0";
       turn1 = true;
     }
@@ -40,25 +38,24 @@ boxes.forEach((box) => {
       // console.log("You draw the match");
       draw();
     }
-
-
-    // box.backgroundColor = "red";
   });
 });
 
-
 let draw = () => {
-  console.log("We have a draw");
-
-}
+  msg.innerHTML = `Ohh! game is a draw`;
+  msg_container.classList.remove("hide");
+  disable_buttons();
+};
 let showWinner = (winner) => {
   msg.innerHTML = `Congratulations winner is ${winner}`;
   msg_container.classList.remove("hide");
-}
+  disable_buttons();
+  // no need to call disable_buttons() as all buttons are clicked and disabled to change state
+  // disable_buttons();
+};
 
 const isWinner = () => {
   for (let pattern of winPatterns) {
-
     let posVal1 = boxes[pattern[0]].innerText;
     let posVal2 = boxes[pattern[1]].innerText;
     let posVal3 = boxes[pattern[2]].innerText;
@@ -75,5 +72,26 @@ const isWinner = () => {
     // console.log(boxes[pattern[0]].innerText , boxes[pattern[1]].innerText , boxes[pattern[2]].innerText)
   }
   // return false; //return undefined automatiacally
+};
+
+const resetGame = () => {
+  count = 0;
+  turn1 = true;
+  enable_buttons();
+  msg_container.classList.add("hide");
+};
+
+function disable_buttons() {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+}
+function enable_buttons() {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerHTML = "";
+  }
 }
 
+resetBtn.addEventListener("click", resetGame);
+new_game.addEventListener("click", resetGame);
